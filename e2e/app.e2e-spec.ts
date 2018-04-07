24cbee9b7cc0c07f430fe8fcf6951669a9b6400e
leopalmeiro <palmeiro.leonardo@gmail.com>
2018-04-07 19:57:13 +0100

    first commit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,5 @@ describe('gerenciados-de-tarefas App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+
 });
